b9f18c23b8c48ea5cb8638a0f60b4f3e6b6184a8
Nicolas Walter <nicowalter25@gmail.com>
2018-05-24 01:13:14 -0300

    ejs 4, 5 y 6

--- Tp 2/esqueleto.pl
+++ Tp 2/esqueleto.pl
@@ -26,9 +26,9 @@ longitudMaxima(E, N) :- symbol(E), N is 1.
 longitudMaxima(concat(E1,E2),N) :- longitudMaxima(E1, N1), longitudMaxima(E2, N2), N is N1+N2.
 longitudMaxima(or(E1,E2),N) :- longitudMaxima(E1,N1), longitudMaxima(E2,N2), N is max(N1,N2).
 
-%% iesimo(0,[X|_],X).
-%% iesimo(I,[_|Xs], Y) :- I \= 0, N is I-1, iesimo(N,Xs,Y).
-%% iesimo2(I,L,X):- length(L,Long), Long2 is Long -1, between(0,Long2,I), iesimo(I,L,X).
+iesimo(0,[X|_],X).
+iesimo(I,[_|Xs], Y) :- I \= 0, N is I-1, iesimo(N,Xs,Y).
+iesimo2(I,L,X):- length(L,Long), Long2 is Long -1, between(0,Long2,I), iesimo(I,L,X).
 
 % Ejercicio 3: cadena(?Cadena)
 
@@ -37,32 +37,22 @@ esCadena([E1|Exps]) :- symbol(E1), cadena(Exps).
 cadena(L) :- length(L, _), esCadena(L).
 
 % Ejercicio 4: match_inst(+Cadena, +RegEx)
-hasta(X, 0). 
-hasta(X, Y):- X is Y-1, hasta(_, X).
 
 generar(empty,[],0).
-generar(E,X,_) :- symbol(E), append([],[E],R), X = R.
-generar(or(E1,E2), X, L) :- generar(E1,X1,L), X = X1 ; generar(E2,X2,L), X = X2.
-generar(concat(E1,E2),X,L) :- L>0, between(0,L,L1), generar(E1,X1,L1), L2 is L-L1, generar(E2,X2,L2), append(X1,X2,X).
+generar(E,X,1) :- symbol(E), append([],[E],X).
+generar(or(E1,E2), X, L) :- generar(E1,X,L), length(X,L); generar(E2,X,L), length(X,L).
 generar(star(E1),X,L) :- length(X,L), (generar(empty,X,L); generar(concat(E1,star(E1)),X,L)).
+generar(concat(E1,E2),X,L) :- length(X,L), append(X2,X1,X), length(X1,L1),length(X2,L2), generar(E1,X1,L1), generar(E2,X2,L2).
 
-generar(empty,[],0).
-generar(E,X,_) :- symbol(E), append([],[E],R), X = R.
-generar(or(E1,E2), X, L) :- generar(E1,X1,L), X = X1 ; generar(E2,X2,L), X = X2.
-generar(concat(E1,E2),X,L) :- L>0, generar(E1,X1,L), M is L-1, generar(E2,X2,M), append(X1,X2,X).
-generar(star(E1),X,L) :- length(X,L), (generar(empty,X,L); generar(concat(E1,star(E1)),X,L)).
 
 match_inst(C, E) :- length(C,L), generar(E,C,L).
-
-match_inst(C, E) :- length(C,L), generar(E,C,L).
-
 
 % Ejercicio 5: match(?Cadena, +RegEx)
-match(_, _) :- fail.
+match(C, E) :- generar(E,C,L).
 
 % Ejercicio 6: diferencia(?Cadena, +RegEx, +RegEx)
 
-diferencia(_, _, _) :- fail.
+diferencia(C, E1, E2) :- match(C,E1), not(match(C,E2)).
 
 % Ejercicio 7: prefijoMaximo(?Prefijo, +Cadena, +RegEx)
 
